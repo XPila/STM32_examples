198bbd6c0d20bd68589f1a0ef098fce9d85e7fcc
Robert Pelnar <robert.pelnar@seznam.cz>
2019-03-20 20:06:38 +0100

    Hex2dfu utility compiled for Windows (MinGW)

---
+++ hex2dfu/ed25519/inttypes.h
@@ -0,0 +1,16 @@
+// inttypes.h
+#ifndef _INTTYPES_H
+#define _INTTYPES_H
+
+#define uint8_t   unsigned char
+#define uint16_t  unsigned short
+#define uint32_t  unsigned long
+#define uint64_t  unsigned long long
+#define int8_t    char
+#define int16_t   short
+#define int32_t   long
+#define int64_t   long long
+
+#define UINT64_C(v) v ##ULL
+
+#endif //_INTTYPES_H
